3e436c04386e4e7ac75a340a90bd6eef15f637d4
sirkudos <attahikechukwu@gmail.com>
2018-06-29 16:32:08 +0100

    initial commit

---
+++ designs.js
@@ -0,0 +1,39 @@
+// Select color input
+// Select size input
+
+var color, width, height;
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event){
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width = $('#inputWeight').val();
+  makeGrid(height, width);
+  
+})
+
+function makeGrid(x, y) {
+  $('tr').remove();
+// Your code goes here!
+for(var i = 1; i<=x; i++){
+    $('#pixelCanvas').append('<tr id = table' +  i + '></tr>');
+
+    for(var j = 1; j<=y; j++){
+        $('#table' + i).append('<td></td>');
+    }
+}
+
+
+
+//adding of color to the cell.
+
+$('td').click(function addColor(){
+    color = $('#colorPicker').val();
+
+    if($(this).attr('style')){
+        $(this).removeAttr('style')
+    }else {
+        $(this).attr('style', 'background-color:' + color)
+    }
+})
+
+}
